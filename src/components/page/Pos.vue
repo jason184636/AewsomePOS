124df2ae539fb969b8c5c0b966ebb4839591947d
jasont <813000816@qq.com>
2017-06-05 23:40:19 +0800

    Initial commit

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pos">
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default{
+        name: 'pos'
+    }
+</script>
+
+<style lang="" rel="stylesheet/text">
+
+</style>
